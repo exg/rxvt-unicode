e25d48b52e39a467d08e0e9753b1e3fff51db9f2
Marc Lehmann <schmorp@schmorp.de>
2005-02-04 04:16:05 +0000

    *** empty log message ***

--- src/logging.C
+++ src/logging.C
@@ -73,17 +73,17 @@ rxvt_term::makeutent (const char *pty, const char *hostname)
   if (!strncmp (pty, "/dev/", 5))
     pty += 5;		/* skip /dev/ prefix */
 
-  if (!strncmp (pty, "pty", 3) || !strncmp (pty, "tty", 3))
-    strncpy (ut_id, (pty + 3), sizeof (ut_id));
 #ifdef HAVE_UTMP_PID
+  if (!strncmp (pty, "pty", 3) || !strncmp (pty, "tty", 3))
+    strncpy (ut_id, pty + 3, sizeof (ut_id));
   else if (sscanf (pty, "pts/%d", &i) == 1)
     sprintf (ut_id, "vt%02x", (i & 0xff));	/* sysv naming */
-#endif
   else if (strncmp (pty, "pty", 3) && strncmp (pty, "tty", 3))
     {
       rxvt_warn ("can't parse tty name \"%s\", not adding utmp entry.\n", pty);
       return;
     }
+#endif
 
 #ifdef HAVE_STRUCT_UTMP
   memset (ut, 0, sizeof (struct utmp));
