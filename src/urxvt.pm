cd5db7fe32bdc3d5a39111ec802355d92d017af1
Marc Lehmann <schmorp@schmorp.de>
2006-01-06 03:43:12 +0000

    *** empty log message ***

--- src/urxvt.pm
+++ src/urxvt.pm
@@ -263,7 +263,7 @@ Called whenever the user presses a key combination that has a
 C<perl:string> action bound to it (see description of the B<keysym>
 resource in the @@RXVT_NAME@@(1) manpage).
 
-=item on_key_press $term, $event
+=item on_key_press $term, $event, $octets
 
 =item on_key_release $term, $event
 
@@ -273,8 +273,12 @@ resource in the @@RXVT_NAME@@(1) manpage).
 
 =item on_motion_notify $term, $event
 
-Called whenever the corresponding X event is received for the terminal. If
-the hook returns true, then the even will be ignored by rxvt-unicode.
+Called whenever the corresponding X event is received for the terminal
+(see the XEvent manpage) If the hook returns true, then the even will be
+ignored by rxvt-unicode.
+
+C<on_key_press> additionally receives the string rxvt-unicode would
+output, if any, in locale-specific encoding.
 
 subwindow.
 
