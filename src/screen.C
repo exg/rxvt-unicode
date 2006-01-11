9e7e3cb231ebb0bc1e6de8a1fe20f39808a64044
Marc Lehmann <schmorp@schmorp.de>
2006-01-11 18:54:30 +0000

    *** empty log message ***

--- src/screen.C
+++ src/screen.C
@@ -574,7 +574,7 @@ rxvt_term::scr_do_wrap ()
 void
 rxvt_term::scr_color (unsigned int color, int fgbg)
 {
-  if (!IN_RANGE_INC (color, minTermCOLOR, maxTermCOLOR))
+  if (!IN_RANGE_INC (color, minCOLOR, maxTermCOLOR))
     color = fgbg;
 
   if (fgbg == Color_fg)
