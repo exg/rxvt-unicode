9b7f4ea99aec014667fad38c771a88d6213c2c09
Emanuele Giaquinta <emanuele.giaquinta@gmail.com>
2006-01-19 10:52:32 +0000

    *** empty log message ***

--- src/feature.h
+++ src/feature.h
@@ -39,6 +39,7 @@
 /* Default mode to restore when releasing the PTS device.  It is relaxed to be
  * compatible with most systems, change it to a more secure value if your
  * system supports it (0640 for example).
+ */
 
 #define RESTORE_TTY_MODE 0666
 
