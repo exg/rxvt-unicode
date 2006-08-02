c146919682b163383e1d78c184a02ddee790978f
Marc Lehmann <schmorp@schmorp.de>
2006-08-02 09:14:19 +0000

    *** empty log message ***

--- src/main.C
+++ src/main.C
@@ -58,15 +58,15 @@ static char curlocale[128], savelocale[128];
 bool
 rxvt_set_locale (const char *locale) NOTHROW
 {
-  int len = strlen (locale) + 1;
+  int size = strlen (locale) + 1;
 
-  if (len > sizeof (curlocale))
+  if (size > sizeof (curlocale))
     rxvt_fatal ("locale string too long, aborting.\n");
 
-  if (!locale || !memcmp (locale, curlocale, len))
+  if (!locale || !memcmp (locale, curlocale, size))
     return false;
 
-  memcpy (curlocale, locale, len);
+  memcpy (curlocale, locale, size);
   setlocale (LC_CTYPE, curlocale);
   return true;
 }
