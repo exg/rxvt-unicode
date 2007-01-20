8b44a3a0cbe2aca3e089825cd4fc52f6d3b2ce56
Marc Lehmann <schmorp@schmorp.de>
2007-01-20 00:40:23 +0000

    *** empty log message ***

--- src/init.C
+++ src/init.C
@@ -1328,7 +1328,7 @@ rxvt_term::run_command (const char *const *argv)
 
   rxvt_get_ttymode (&tio, er);
   pty->set_utf8_mode (enc_utf8);
-  SET_TTYMODE (STDIN_FILENO, &tio);       /* init terminal attributes */
+  SET_TTYMODE (pty->tty, &tio);       /* init terminal attributes */
 
   /* set initial window size */
   tt_winch ();
