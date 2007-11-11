00800f3f8e5857c576dfecb656b0444c36ea4982
Marc Lehmann <schmorp@schmorp.de>
2007-11-11 16:28:46 +0000

    *** empty log message ***

---
+++ src/ev_cpp.C
@@ -0,0 +1,7 @@
+#include "rxvttoolkit.h"
+#include "ev++.C"
+
+namespace ev {
+  #include "ev.c"
+}
+
---
+++ src/ev_cpp.h
@@ -0,0 +1,5 @@
+#define EV_MULTIPLICITY 0
+#define EV_PERIODICS 0
+#define EV_USE_SELECT 0
+#define EV_USE_POLL 1
+#include "ev++.h"
