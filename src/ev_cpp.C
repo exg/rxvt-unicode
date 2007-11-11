f3ca23cc7cfeb90c40bb2cbda69b08f2bcaf3931
Marc Lehmann <schmorp@schmorp.de>
2007-11-11 16:45:38 +0000

    *** empty log message ***

--- src/ev_cpp.C
+++ src/ev_cpp.C
@@ -1,7 +1,8 @@
 #include "rxvttoolkit.h"
 #include "ev++.C"
 
-namespace ev {
-  #include "ev.c"
-}
+/* darwin has problems with its header files in C++, requiring this namespace juggling */
+using namespace ev;
+
+#include "ev.c"
 
