fa4fa8ff1890e8f3659dc9acd771ad1bab596372
Marc Lehmann <schmorp@schmorp.de>
2007-11-22 05:40:31 +0000

    disable kqueue

--- src/ev_cpp.h
+++ src/ev_cpp.h
@@ -1,4 +1,11 @@
 #define EV_USE_POLL 0
 #define EV_MULTIPLICITY 0
 #define EV_PERIODICS 0
+
+#ifndef __NetBSD__
+// disable kqueue on anything but netbsd, as its broken
+// on freebsd, openbsd and darwin
+# define EV_USE_KQUEUE 0
+#endif
+
 #include "ev++.h"
