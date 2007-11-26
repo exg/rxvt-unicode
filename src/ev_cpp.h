908b7c258deaf2657707c37c8a284d62620c2132
Marc Lehmann <schmorp@schmorp.de>
2007-11-26 18:15:32 +0000

    seems per-type coinfigurability comes back...

--- src/ev_cpp.h
+++ src/ev_cpp.h
@@ -1,6 +1,8 @@
 #define EV_USE_POLL 0
 #define EV_MULTIPLICITY 0
 #define EV_PERIODIC_ENABLE 0
+#define EV_EMBED_ENABLE 0
+#define EV_PATH_ENABLE 0
 #define EV_CONFIG_H <config.h>
 #define EV_MINPRI 0
 #define EV_MAXPRI 0
