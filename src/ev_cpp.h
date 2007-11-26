80c05aedbdd64b7e2e0d4b92a034e101018443e5
Marc Lehmann <schmorp@schmorp.de>
2007-11-26 18:29:55 +0000

    pleasedonotask

--- src/ev_cpp.h
+++ src/ev_cpp.h
@@ -1,8 +1,7 @@
 #define EV_USE_POLL 0
 #define EV_MULTIPLICITY 0
 #define EV_PERIODIC_ENABLE 0
-#define EV_EMBED_ENABLE 0
-#define EV_PATH_ENABLE 0
+#define EV_STAT_ENABLE 0
 #define EV_CONFIG_H <config.h>
 #define EV_MINPRI 0
 #define EV_MAXPRI 0
