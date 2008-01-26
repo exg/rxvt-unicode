7e455a47292bbb26b27b00dbf0f0222d75a4b3d8
Marc Lehmann <schmorp@schmorp.de>
2008-01-26 12:03:36 +0000

    *** empty log message ***

--- src/rxvttoolkit.C
+++ src/rxvttoolkit.C
@@ -571,12 +571,15 @@ void rxvt_display::x_cb (ev::io &w, int revents)
             }
         }
     }
+
+  flush_ev.start ();
 }
 
 void rxvt_display::flush_cb (ev::prepare &w, int revents)
 {
-  w.stop ();
+  x_cb(x_ev,0);//D
   XFlush (dpy);
+  w.stop ();
 }
 
 void rxvt_display::reg (xevent_watcher *w)
