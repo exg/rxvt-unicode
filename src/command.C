beeb5b32e7d13f1b5413ed93ab4e1a4470667c06
Emanuele Giaquinta <emanuele.giaquinta@gmail.com>
2010-08-18 17:45:42 +0000

    Encode modifiers in 'CSI Ps ~' sequences _before_ invoking the on_key_press hook.

--- src/command.C
+++ src/command.C
@@ -773,7 +773,19 @@ rxvt_term::key_press (XKeyEvent &ev)
                 {
                   int param = map_function_key (keysym);
                   if (param > 0)
-                    sprintf (kbuf,"\033[%d~", param);
+                    {
+                      int n = sprintf (kbuf,"\033[%d~", param);
+                      /*
+                       * pass Shift/Control indicators for function keys ending with `~'
+                       *
+                       * eg,
+                       *   Prior = "ESC[5~"
+                       *   Shift+Prior = "ESC[5$"
+                       *   Ctrl+Prior = "ESC[5^"
+                       *   Ctrl+Shift+Prior = "ESC[5@"
+                       */
+                      kbuf[n-1] = (shft ? (ctrl ? '@' : '$') : (ctrl ? '^' : '~'));
+                    }
                   else
                     newlen = 0;
                 }
@@ -834,18 +846,8 @@ rxvt_term::key_press (XKeyEvent &ev)
       }
 
   /*
-   * these modifications only affect the static keybuffer
-   * pass Shift/Control indicators for function keys ending with `~'
-   *
-   * eg,
-   *   Prior = "ESC[5~"
-   *   Shift+Prior = "ESC[5$"
-   *   Ctrl+Prior = "ESC[5^"
-   *   Ctrl+Shift+Prior = "ESC[5@"
    * Meta adds an Escape prefix (with META8_OPTION, if meta == <escape>).
    */
-  if (kbuf[0] == C0_ESC && kbuf[1] == '[' && kbuf[len - 1] == '~')
-    kbuf[len - 1] = (shft ? (ctrl ? '@' : '$') : (ctrl ? '^' : '~'));
 
   /* escape prefix */
   if (meta
