7861bdff2cfb3c9ca0ccd0960dfcccf1c2f3696f
Marc Lehmann <schmorp@schmorp.de>
2012-06-16 15:55:19 +0000

    *** empty log message ***

--- src/rxvtimg.h
+++ src/rxvtimg.h
@@ -1,4 +1,27 @@
-#ifndef IMG_H),
+/*----------------------------------------------------------------------*
+ * File:	rxvtimg.C
+ *----------------------------------------------------------------------*
+ *
+ * All portions of code are copyright by their respective author/s.
+ * Copyright (c) 2012      Marc Lehmann <schmorp@schmorp.de>
+ * Copyright (c) 2012      Emanuele Giaquinta <e.giaquinta@glauco.it>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *---------------------------------------------------------------------*/
+
+#ifndef IMG_H
 #define IMG_H
 
 #if HAVE_BG_PIXMAP
